8507a51a3deba9d19998905da0e0a347c334e52e
Iustin6x <iustinserian@gmail.com>
2021-07-13 18:54:04 +0300

    tracking route

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,11 +12,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
 
+import { IonicStorageModule } from '@ionic/storage-angular'
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(),IonicStorageModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
